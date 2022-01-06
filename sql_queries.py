371fe8ce9ec5fd64521eb16377ff2f437341e432
Begen Serzhan <42128545+begen@users.noreply.github.com>
2022-01-06 18:23:25 -0500

    Added empty files

---
+++ sql_queries.py
@@ -0,0 +1,58 @@
+# DROP TABLES
+
+songplay_table_drop = ""
+user_table_drop = ""
+song_table_drop = ""
+artist_table_drop = ""
+time_table_drop = ""
+
+# CREATE TABLES
+
+songplay_table_create = ("""
+
+""")
+
+user_table_create = ("""
+""")
+
+song_table_create = ("""
+""")
+
+artist_table_create = ("""
+""")
+
+time_table_create = ("""
+""")
+
+# INSERT RECORDS
+
+songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent
+
+
+songplay_table_insert = ("""
+CREATE TABLE IF NOT EXISTS music_library(songplay_id int, start_time DATETIME NOT NULL, user_id int, level, song_id, artist_id, session_id, location, user_agent)
+
+""")
+
+user_table_insert = ("""
+""")
+
+song_table_insert = ("""
+""")
+
+artist_table_insert = ("""
+""")
+
+
+time_table_insert = ("""
+""")
+
+# FIND SONGS
+
+song_select = ("""
+""")
+
+# QUERY LISTS
+
+create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
