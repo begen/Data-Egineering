7fa5bdc756255bb2f687adf4a4aba4406cf54c06
Begen Serzhan <42128545+begen@users.noreply.github.com>
2022-01-20 09:55:57 -0500

    Automated data pipeline

--- sql_queries.py
+++ sql_queries.py
@@ -11,25 +11,23 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 # CREATE TABLES
 
 songplay_table_create = ("""
-    CREATE TABLE IF NOT EXISTS songplays
-    (
-        songplay_id serial,
-        start_time date NOT NULL,
-        user_id int NOT NULL,
-        level varchar NOT NULL,
-        song_id varchar,
-        artist_id varchar,
-        session_id int,
-        location varchar,
-        user_agent text 
-     );
-    
+CREATE TABLE IF NOT EXISTS songplays (
+    songplay_id SERIAL PRIMARY KEY,
+    start_time BIGINT,
+    user_id VARCHAR,
+    level VARCHAR,
+    song_id VARCHAR,
+    artist_id VARCHAR,
+    session_id VARCHAR,
+    location VARCHAR,
+    user_agent VARCHAR
+);
 """)
 
 user_table_create = ("""
     CREATE TABLE IF NOT EXISTS users
     (
-        user_id int PRIMARY KEY,
+        user_id VARCHAR PRIMARY KEY,
         first_name varchar NOT NULL,
         last_name varchar,
         gender char(1) NOT NULL,
@@ -60,24 +58,23 @@ artist_table_create = ("""
 """)
 
 time_table_create = ("""
-    CREATE TABLE IF NOT EXISTS time 
-    (
-        start_time date PRIMARY KEY,
-        hour int NOT NULL,
-        day int NOT NULL,
-        week int NOT NULL,
-        month int NOT NULL,
-        year int NOT NULL,
-        weekday text 
-    ) 
+CREATE TABLE IF NOT EXISTS time (
+    start_time TIMESTAMP PRIMARY KEY, 
+    hour INT NOT NULL, 
+    day INT NOT NULL, 
+    week INT NOT NULL, 
+    month INT NOT NULL, 
+    year INT NOT NULL, 
+    weekday INT NOT NULL
+);
 """)
 
 # INSERT RECORDS
 
 songplay_table_insert = ("""
     INSERT INTO songplays
-    (songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
-     VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s);
+    (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
+     VALUES (%s,%s,%s,%s,%s,%s,%s,%s);
 """)
 
 user_table_insert = ("""
@@ -125,14 +122,15 @@ time_table_insert = ("""
 # FIND SONGS
 
 song_select = ("""
-    SELECT song_id, artists.artist_id
-    FROM songs JOIN artists ON songs.artist_id = artists.artist_id
-    WHERE songs.title = %s
-    AND artists.name = %s
-    AND songs.duration = %s
+    SELECT s.song_id, a.artist_id
+    FROM songs s 
+    JOIN artists a ON s.artist_id = a.artist_id
+    WHERE s.title = %s
+    AND a.name = %s
+    AND s.duration = %s
 """)
 
 # QUERY LISTS
 
-create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+create_table_queries = [user_table_create, song_table_create, artist_table_create, time_table_create,songplay_table_create]
 drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
